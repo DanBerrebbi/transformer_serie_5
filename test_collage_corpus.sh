a8d30d4f2f5d37de0c2f82dc530ecad80d3c3713
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-15 16:29:26 +0200

    essai

--- test_collage_corpus.sh
+++ test_collage_corpus.sh
@@ -12,11 +12,11 @@ stovec=$josep/stovec
 
 dir=$josep/minmt_base
 
-dir_dan = /data_corpus
+mkdir "data_corpus"
 dnet=$PWD/model_serie_true
 
 
-	for corpus in ECB ; do
+	for corpus in ECB EMEA Europarl; do
 	    echo $corpus
 
 	    fsrc=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.src
@@ -24,10 +24,10 @@ dnet=$PWD/model_serie_true
 	    fsim=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.sim
 	    fpre=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.pre
 
-	    cat $fsrc >> trn_src
-	    cat $ftgt >> $dir_dan/trn_tgt
-	    cat $fsim >> $dir_dan/trn_sim
-	    cat $fpre >> $dir_dan/trn_pre
+	    cat $fsrc >> data_corpus/trn_src
+	    cat $ftgt >> data_corpus/trn_tgt
+	    cat $fsim >> data_corpus/trn_sim
+	    cat $fpre >> data_corpus/trn_pre
    echo ok
 
   done
