0bb5395961520ee6fa93e58bdf5f707fae3b58ea
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-16 14:44:18 +0200

    get bleu score

---
+++ sacrebleu.sh
@@ -0,0 +1,10 @@
+
+
+#dnet=$PWD/model_serie
+dnet=$PWD/model_checkpoint
+josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
+data=$josep/data
+
+input = $dnet/$corpus.out_k5_avg_alpha0.7
+
+$input > python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > $input.bleu
