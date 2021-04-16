52561d04b056d47eb98c1b245a59f1da5be7be52
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-16 15:24:20 +0200

    get bleu score

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -16,4 +16,4 @@ done
 
 #cp model_checkpoint/Europarl.out_k5_avg_alpha0.7 Europarl.out_k5_avg_alpha0.7.trans
 
-$PWD/Europarl.out_k5_avg_alpha0.7.trans >> python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force model_checkpoint/reference > model_checkpoint/Europarl.out_k5_avg_alpha0.7.bleu
+model_checkpoint/Europarl.out_k5_avg_alpha0.7 > python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force model_checkpoint/reference > model_checkpoint/Europarl.out_k5_avg_alpha0.7.bleu
