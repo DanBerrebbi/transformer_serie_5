8c945df8a65ec1151a4c03e89b36d9883590cd28
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-16 15:01:04 +0200

    get bleu score

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -5,15 +5,13 @@ dnet=$PWD/model_checkpoint
 josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 data=$josep/data
 
-input = $dnet/Europarl.out_k5_avg_alpha0.7    # il s'appelle Europarl mais ya les 3 premiers corpus dedans
-fref = $dnet/reference
+input = model_checkpoint/Europarl.out_k5_avg_alpha0.7    # il s'appelle Europarl mais ya les 3 premiers corpus dedans
 
 # jointure fichiers de reference :
 for corpus in  ECB EMEA Europarl ; do
-  cat $data/clean.Europarl.en-fr.fr.tst >> $fref
-
+  cat $data/clean.Europarl.en-fr.fr.tst >> model_checkpoint/reference
 
 done
 
 
-$input > python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > $input.bleu
+$input > python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force model_checkpoint/reference > $input.bleu
