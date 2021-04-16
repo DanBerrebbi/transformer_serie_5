ebf485dfd38816d170b54d518d5251391f0a3d92
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-16 15:18:35 +0200

    get bleu score

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -14,5 +14,6 @@ for corpus in  ECB EMEA Europarl ; do
 
 done
 
+cp model_checkpoint/Europarl.out_k5_avg_alpha0.7 model_checkpoint/Europarl.out_k5_avg_alpha0.7.trans
 
-model_checkpoint/Europarl.out_k5_avg_alpha0.7 >> python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force model_checkpoint/reference > model_checkpoint/Europarl.out_k5_avg_alpha0.7.bleu
+model_checkpoint/Europarl.out_k5_avg_alpha0.7.trans >> python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force model_checkpoint/reference > model_checkpoint/Europarl.out_k5_avg_alpha0.7.bleu
