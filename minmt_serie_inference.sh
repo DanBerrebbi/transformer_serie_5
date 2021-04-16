43f5170b9c16f27f31f24799583c41a0ac23310a
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-16 10:56:22 +0200

    syntax error fixed

--- minmt_serie_inference.sh
+++ minmt_serie_inference.sh
@@ -21,10 +21,10 @@ dnet=$PWD/model_checkpoint
 	for corpus in ECB EMEA Europarl ; do
 	    echo $corpus
 	    echo test ...
-	    fsrc_tst=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.src
-	    ftgt_tst=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.tgt
-	    fsim_tst=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.sim
-	    fpre_tst=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.pre
+	    fsrc=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.src
+	    ftgt=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.tgt
+	    fsim=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.sim
+	    fpre=$stovec/clean.Europarl.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.pre
 	    cat $fsrc >> data_corpus/tst_src
 	    cat $ftgt >> data_corpus/tst_tgt
 	    cat $fsim >> data_corpus/tst_sim
