5c36978cc69e46f08ad58a5c10266f42cd322d8e
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-19 10:55:28 +0200

    inference serie finale

--- minmt_serie_inference.sh
+++ minmt_serie_inference.sh
@@ -31,7 +31,7 @@ fmod=$dnet/network.checkpoint_00450000.pt ####### A MODIF
 	    fsim=$stovec/clean.$corpus.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.sim
 	    fpre=$stovec/clean.$corpus.en-fr.en.tst.bpe.vec.sim0.5_k5_n0_t0.8.pre
       fout=$dnet/$corpus.out_k5_alpha0.7
-      CUDA_VISIBLE_DEVICES=0 python3 $trans -dnet $dnet -m $fmod -batch_size 10 -beam_size 5 -alpha 0.7 -i_src data_corpus/tst_src -i_sim data_corpus/tst_sim -i_pre data_corpus/tst_pre -o $fout -cuda -log_file $fout.log &
+      CUDA_VISIBLE_DEVICES=0 python3 $trans -dnet $dnet -m $fmod -batch_size 10 -beam_size 5 -alpha 0.7 -i_src $fsrc -i_sim $fsim -i_pre $fpre -o $fout -cuda -log_file $fout.log &
 
 	    echo test ok
 
