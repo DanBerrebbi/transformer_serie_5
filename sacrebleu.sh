7f173e28aa7494f63b39271a55e738acf7d75db1
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-19 11:15:56 +0200

    sacrebleu premiere

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -4,18 +4,7 @@
 dnet=$PWD/model_checkpoint
 josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 data=$josep/data
-tokenizer=$josep/MiNMT/tools/tokenizer.py
+tokenizer=tools/tokenizer.py
 
-   # il s'appelle Europarl mais ya les 3 premiers corpus dedans
 
-# jointure fichiers de reference :
-for corpus in  ECB EMEA Europarl ; do
-  cat $data/clean.$corpus.en-fr.fr.tst > model_checkpoint/reference
-
-done
-
-#cp model_checkpoint/Europarl.out_k5_avg_alpha0.7 Europarl.out_k5_avg_alpha0.7.trans
-# model_checkpoint/Europarl.out_k5_avg_alpha0.7
-cat model_checkpoint/Europarl.out_k5_avg_alpha0.7 | python3 $tokenizer -tok_config $data/BPE_config -detok > model_checkpoint/Europarl.out_k5_avg_alpha0.7.trans
-
-#| sacrebleu --force model_checkpoint/reference > model_checkpoint/Europarl.out_k5_avg_alpha0.7.bleu
+sort -g model_serie/EMEA.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/EMEA.out_k5_alpha0.7.bleu
