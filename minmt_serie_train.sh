29f302d85d5ae2a7f4894027c1adc9f05dec7e3b
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-20 10:47:13 +0200

    nouveau train script

--- minmt_serie_train.sh
+++ minmt_serie_train.sh
@@ -5,7 +5,7 @@ josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 
 vocab=$PWD/minmt-vocab.py
 setup=$PWD/minmt-setup.py
-train=$PWD/minmt-train.py      # j'ai modifié certains fichiers par rapport au Git
+train=$PWD/minmt-train.py
 avrge=$PWD/minmt-average.py
 trans=$PWD/minmt-translate.py
 
@@ -13,53 +13,14 @@ tokenizer=$josep/MiNMT/tools/tokenizer.py
 data=$josep/data
 stovec=$josep/stovec
 
-echo tokenizer
-cat $setup
-
-    dir=$josep/minmt_base
-    dnet=$PWD/model_serie
-
-    if true; then
-	python3 $setup -dnet $dnet -src_voc $dir/enfr.BPE.32k.voc -tgt_voc $dir/enfr.BPE.32k.voc
-
-	echo setup ok
-
-mkdir "data_corpus"
-
-	for corpus in ECB EMEA Europarl GNOME JRC-Acquis KDE4 news-commentary-v14 TED2013 Wikipedia; do
-	    echo $corpus
-      echo train ...
-	    fsrc=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.src
-      ftgt=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.tgt
-	    fsim=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.sim
-	    fpre=$stovec/clean.$corpus.en-fr.en.trn.bpe.vec.max_sim0.5_k5_n0_t0.8.pre
-	    cat $fsrc >> data_corpus/trn_src
-	    cat $ftgt >> data_corpus/trn_tgt
-	    cat $fsim >> data_corpus/trn_sim
-	    cat $fpre >> data_corpus/trn_pre
-	    echo train ok
-
-	    echo val ...
-	    fsrc=$stovec/clean.$corpus.en-fr.en.val.bpe.vec.sim0.5_k5_n0_t0.8.src
-	    ftgt=$stovec/clean.$corpus.en-fr.en.val.bpe.vec.sim0.5_k5_n0_t0.8.tgt
-	    fsim=$stovec/clean.$corpus.en-fr.en.val.bpe.vec.sim0.5_k5_n0_t0.8.sim
-	    fpre=$stovec/clean.$corpus.en-fr.en.val.bpe.vec.sim0.5_k5_n0_t0.8.pre
-	    cat $fsrc >> data_corpus/val_src
-	    cat $ftgt >> data_corpus/val_tgt
-	    cat $fsim >> data_corpus/val_sim
-	    cat $fpre >> data_corpus/val_pre
-	    echo val ok
-
-   echo corpus suivant :
-
-  done
+dir=$josep/minmt_base
+dnet=$PWD/model_serie
 
 
+python3 $setup -dnet $dnet -src_voc $dir/enfr.BPE.32k.voc -tgt_voc $dir/enfr.BPE.32k.voc
+echo setup ok
+echo debut du training
 
-  	echo debut du training
+CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src  -sim_train data_corpus/trn_sim  -pre_train data_corpus/trn_pre  -tgt_train data_corpus/trn_tgt  -src_valid data_corpus/val_src  -sim_valid data_corpus/val_sim  -pre_valid data_corpus/val_pre  -tgt_valid data_corpus/val_tgt -max_steps 450000 -loss KLDiv -cuda -log_file $dnet/log &
 
-	CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src  -sim_train data_corpus/trn_sim  -pre_train data_corpus/trn_pre  -tgt_train data_corpus/trn_tgt  -src_valid data_corpus/val_src  -sim_valid data_corpus/val_sim  -pre_valid data_corpus/val_pre  -tgt_valid data_corpus/val_tgt -max_steps 450000 -loss KLDiv -cuda -log_file $dnet/log &
-
-		echo fin du training
-
-fi
+echo fin du training
