885b42caabb976102b3f4739dd54f85fc1187d6f
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-21 19:34:43 +0200

    inference checkpoint changed

--- minmt_serie_inference.sh
+++ minmt_serie_inference.sh
@@ -17,7 +17,7 @@ dir=$josep/minmt_base
 dnet=$PWD/model_serie
 
 
-fmod=$dnet/network.checkpoint_00005000.pt ####### A MODIF
+fmod=$dnet/network.checkpoint_00025000.pt ####### A MODIF
 
 	for corpus in Europarl; do
 	    echo $corpus
