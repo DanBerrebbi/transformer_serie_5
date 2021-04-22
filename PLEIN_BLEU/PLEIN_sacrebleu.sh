2aaeb78b5eace6ccc43ccf0cbe237bdb5eb36d8d
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 17:05:43 +0200

    petits cgt

--- PLEIN_BLEU/PLEIN_sacrebleu.sh
+++ PLEIN_BLEU/PLEIN_sacrebleu.sh
@@ -7,5 +7,5 @@ data=$josep/data
 tokenizer=../tools/tokenizer.py
 fref=$data/clean.Europarl.en-fr.fr.tst
 
-
-sort -g pred.tst | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force ref.tst > Europarl_plein.bleu
+sort -g pred.tst | cut -f 2 > pred.trans
+#sort -g pred.tst | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force ref.tst > Europarl_plein.bleu
