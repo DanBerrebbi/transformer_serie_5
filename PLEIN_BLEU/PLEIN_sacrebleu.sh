c0f2d151f1424de348321325861ed684da8a8d4a
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 16:48:25 +0200

    sacrebleu plein

--- PLEIN_BLEU/PLEIN_sacrebleu.sh
+++ PLEIN_BLEU/PLEIN_sacrebleu.sh
@@ -7,6 +7,6 @@ data=$josep/data
 tokenizer=../tools/tokenizer.py
 fref=$data/clean.Europarl.en-fr.fr.tst
 
-ref | python3 $tokenizer -tok_config $data/BPE_config -detok > ref_detok
+$PWD/ref | python3 $tokenizer -tok_config $data/BPE_config -detok > ref_detok
 
-sort -g ref | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $ref_detok > Europarl_plein.bleu
+#sort -g /ref | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $ref_detok > Europarl_plein.bleu
