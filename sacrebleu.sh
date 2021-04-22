441fa15a83d1e8e762a40917271855b3b1186be8
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 10:41:59 +0200

    Europarl

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -5,6 +5,6 @@ dnet=$PWD/model_checkpoint
 josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 data=$josep/data
 tokenizer=tools/tokenizer.py
-fref=$data/clean.ECB.en-fr.fr.tst
+fref=$data/clean.Europarl.en-fr.fr.tst
 
-sort -g model_serie/ECB.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
+sort -g model_serie/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
