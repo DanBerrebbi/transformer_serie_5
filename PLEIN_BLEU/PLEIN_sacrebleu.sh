e68ceb1cad63340cf6b6aaab9e483ed4297c31ef
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-22 16:45:07 +0200

    sacrebleu plein

---
+++ PLEIN_BLEU/PLEIN_sacrebleu.sh
@@ -0,0 +1,12 @@
+
+
+#dnet=$PWD/model_serie
+dnet=$PWD/model_checkpoint
+josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
+data=$josep/data
+tokenizer=tools/tokenizer.py
+fref=$data/clean.Europarl.en-fr.fr.tst
+
+ref | python3 $tokenizer -tok_config $data/BPE_config -detok > ref_detok
+
+sort -g ref | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $ref_detok > model_serie/Europarl.out_k5_alpha0.7.bleu
