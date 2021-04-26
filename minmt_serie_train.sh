ebc280c7e421f18424d793ce28f4ce2c631164d5
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-26 17:01:00 +0200

    retrain after 450k

--- minmt_serie_train.sh
+++ minmt_serie_train.sh
@@ -14,13 +14,13 @@ data=$josep/data
 stovec=$josep/stovec
 
 dir=$josep/minmt_base
-dnet=$PWD/model_serie_plein
+dnet=$PWD/model_serie
 
 
-python3 $setup -dnet $dnet -src_voc $dir/enfr.BPE.32k.voc -tgt_voc $dir/enfr.BPE.32k.voc
+#python3 $setup -dnet $dnet -src_voc $dir/enfr.BPE.32k.voc -tgt_voc $dir/enfr.BPE.32k.voc
 echo setup ok
 echo debut du training
 
-CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src_plein  -sim_train data_corpus/trn_sim_plein  -pre_train data_corpus/trn_pre_plein  -tgt_train data_corpus/trn_tgt_plein  -src_valid data_corpus/val_src_plein  -sim_valid data_corpus/val_sim_plein  -pre_valid data_corpus/val_pre_plein  -tgt_valid data_corpus/val_tgt_plein -max_steps 450000 -loss KLDiv -cuda -log_file $dnet/log &
+CUDA_VISIBLE_DEVICES=0 python3 $train -dnet $dnet -src_train data_corpus/trn_src  -sim_train data_corpus/trn_sim  -pre_train data_corpus/trn_pre  -tgt_train data_corpus/trn_tgt  -src_valid data_corpus/val_src  -sim_valid data_corpus/val_sim  -pre_valid data_corpus/val_pre  -tgt_valid data_corpus/val_tgt -max_steps 300000 -loss KLDiv -cuda -log_file $dnet/log &
 
 echo fin du training
