676b57452b3a8fab20a247b4b2baaecdddc50bec
dan berrebbi <dan.berrebbi@polytechnique.edu>
2021-04-26 11:59:37 +0200

    sort

--- sort.sh
+++ sort.sh
@@ -1,3 +1,3 @@
-sort -g model_serie/ECB.out_k5_alpha0.7 > PLEIN_BLEU/ECB.out_k5_alpha0.7.sorted
+sort -g model_serie_plein/ECB.out_k5_alpha0.7 > PLEIN_BLEU/ECB.out_k5_alpha0.7.sorted
 
 python3 remove_empty_predictions.py
