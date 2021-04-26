3c0cea938a961b9b8f2d0c665ac6c48a7a7676df
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-26 10:23:59 +0200

    sacreplein

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -7,4 +7,4 @@ data=$josep/data
 tokenizer=tools/tokenizer.py
 fref=$data/clean.Europarl.en-fr.fr.tst
 
-sort -g model_serie/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
+sort -g model_serie_plein/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie_plein/Europarl.out_k5_alpha0.7.bleu
