00443446f8c26d90e5a2b5f469086f1127b9cba8
dan berrebbi <dan.berrebbi@polytechnique.edu>
2021-04-26 09:09:34 +0000

    conflict

--- sort.sh
+++ sort.sh
@@ -1,1 +1,1 @@
-sort -g model_serie/Europarl.out_k5_alpha0.7 > PLEIN_BLEU/Europarl.out_k5_alpha0.7.sorted
+sort -g model_serie_plein/Europarl.out_k5_alpha0.7 > PLEIN_BLEU/Europarl.out_k5_alpha0.7.sorted
