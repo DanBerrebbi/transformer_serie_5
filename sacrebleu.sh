c666324fda03ee46036f0aff16b817e87ded4ac5
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-26 11:04:17 +0200

    plein

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -7,6 +7,6 @@ data=$josep/data
 tokenizer=tools/tokenizer.py
 fref=$data/clean.ECB.en-fr.fr.tst
 
-sort -g model_serie/ECB.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/ECB.out_k5_alpha0.7.bleu
+sort -g model_serie_plein/ECB.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie_plein/ECB.out_k5_alpha0.7.bleu
 
-cat model_serie/ECB.out_k5_alpha0.7.bleu
+cat model_serie_plein/ECB.out_k5_alpha0.7.bleu
