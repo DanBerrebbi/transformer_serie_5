dbb5f711d1f4e4cd15ec87d87500554e16190249
Dan BERREBBI <dan.berrebbi@polytechnique.edu>
2021-04-27 10:27:19 +0200

    cgt ckp

--- sacrebleu.sh
+++ sacrebleu.sh
@@ -3,8 +3,8 @@
 josep=/nfs/RESEARCH/crego/projects/PrimingNMT-2/
 data=$josep/data
 tokenizer=tools/tokenizer.py
-fref=$data/clean.news-commentary-v14.en-fr.fr.tst
+fref=$data/clean.Europarl.en-fr.fr.tst
 
-sort -g model_serie_plein/news-commentary-v14.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie_plein/news-commentary-v14.out_k5_alpha0.7.bleu
+sort -g model_serie/Europarl.out_k5_alpha0.7 | cut -f 2 | python3 $tokenizer -tok_config $data/BPE_config -detok | sacrebleu --force $fref > model_serie/Europarl.out_k5_alpha0.7.bleu
 
-cat model_serie_plein/news-commentary-v14.out_k5_alpha0.7.bleu
+cat model_serie/Europarl.out_k5_alpha0.7.bleu
